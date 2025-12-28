01fce21a7e5e8379ebb065d1efc0a48a9a7d241d
sandeepmishra.work <sandeepmishra.work@gmail.com>
2025-12-28 17:49:16 +0530

    refactor

--- main.tf
+++ main.tf
@@ -1,20 +1,10 @@
-
-resource "tls_private_key" "for_remote_exec" {
-  algorithm = "ED25519"
-}
-
-resource "aws_key_pair" "for_remote_exec" {
-  key_name = "remote-exec-key"
-  public_key = tls_private_key.for_remote_exec.public_key_openssh
-}
-
 resource "aws_instance" "test_remote_exec" {
-  ami                    = "ami-00ca570c1b6d79f36"
-  instance_type          = "t3.small"
+  ami                    = var.ami
+  instance_type          = var.instance_type
   key_name               = aws_key_pair.for_remote_exec.key_name
   vpc_security_group_ids = [aws_security_group.web_sg.id]
   tags = {
-    Name       = "nginx-web-server"
+    Name       = var.name
     Managed_by = "Terraform"
     OS_Type    = "Linux(amazon linux 2023)"
   }
--- variables.tf
+++ variables.tf
@@ -1,4 +1,19 @@
+variable "name" {
+  type = string
+  default = "nginx-web-server"
+}
+
 variable "ports" {
   type = list(string)
   default = [22, 80, 443, 8080]
+}
+
+variable "ami" {
+  type = string
+  default = "ami-00ca570c1b6d79f36"
+}
+
+variable "instance_type" {
+  type = string
+  default = "t3.small"
 }
